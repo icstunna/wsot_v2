ad5b9ea11f73fa6f727f64ba9a7ffa06ae23da7e
icstunna <mdelostr@gmail.com>
2016-10-22 14:42:26 -0700

    journals form has been completed along with show page

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,12 +1,22 @@
 class JournalsController < ApplicationController
   
   def new
+    @portfolio = Portfolio.find(params[:portfolio_id].to_i)
+    @position = Position.find(params[:position_id].to_i)
+    @journal = Journal.new
   end
   
   def create
+    @position = Position.find(params[:position_id])
+    @journal = Journal.new(journal_params)
+    @journal.position_id = @position.id
+    @journal.save
+    
+    redirect_to portfolio_position_url(portfolio_id: params[:portfolio_id], id: @position.id)
   end
   
   def show
+    @journal = Journal.find(params[:id])
   end
   
   def edit
@@ -16,5 +26,11 @@ class JournalsController < ApplicationController
   end
   
   def delete
+  end
+  
+  private
+  
+  def journal_params
+    params.require(:journal).permit(:title, :entry)
   end
 end
